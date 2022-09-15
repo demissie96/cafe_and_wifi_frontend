365d71f7c8bd60ed9ea68127530bf2fbf3c662e0
Johannes Demissie <demissie.johannes@gmail.com>
2022-09-15 20:17:03 +0200

    Connect to backend with axios.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import Header from "./Header";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+var cafeList;
+var listElement = [];
+var renderList;
 
 function App() {
+  const [list, setList] = useState('')
+  useEffect(() => {
+    axios.get("http://localhost:8080/").then(function (response) {
+      cafeList = response.data;
+      // console.log(cafeList);
+      cafeList.forEach((element) => {
+        // console.log(element.name);
+        listElement.push(element);
+      });
+      console.log(listElement);
+      renderList = listElement.map((element) => {
+        return <li key={element.id}>{element.name}</li>;
+      });
+      setList(renderList)
+    });
+  }, []);
+
   return (
     <>
       <Header />
+      <div id="cafe-list" style={{ marginTop: "20px" }}>
+        <ul>{list}</ul>
+      </div>
     </>
   );
 }
